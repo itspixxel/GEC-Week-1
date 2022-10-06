5c5616e67810f6e04dae982fdd1b730759dba29b
itspixxel <s029518m@student.staffs.ac.uk>
2022-10-06 16:49:25 +0100

    Hello World

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -1,11 +1,13 @@
-// cppBasics.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
