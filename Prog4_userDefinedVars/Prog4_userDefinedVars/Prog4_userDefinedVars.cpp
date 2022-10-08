fa5c55aeed041fa80fee28159af078f95a7d6d81
itspixxel <s029518m@student.staffs.ac.uk>
2022-10-08 06:26:49 +0100

    Added Prog4_userDefinedVars

---
+++ Prog4_userDefinedVars/Prog4_userDefinedVars/Prog4_userDefinedVars.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	typedef int health;
+	
+	enum Weapons
+	{
+		SWORD = 1,
+		DAGGER,
+		MACE,
+		TWIN_SWORDS,
+		SAMURAI,
+		WIZARD_STAFF,
+		FIRE_POTION,
+		ICE_BLADE,
+		SMALL_KNIFE
+	};
+
+	health Health = 1300;
+	Weapons Weapon = SAMURAI;
+
+	cout << "My current health is " << Health << endl;
+	cout << "And the ID of my weapon of choice is " << Weapon << endl;
+
+	return 0;
+}
