80321f5065d4a9bb4e3098385a87fcfea0445d17
itspixxel <s029518m@student.staffs.ac.uk>
2022-10-08 19:41:04 +0100

    Added Prog6_area

---
+++ Prog6_area/Prog6_area/Prog6_area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int width = 2;
+	int height = 22;
+	int area = width * height;
+
+	cout << "The area of a " << width << " by " << height << "rectangle is" << area << endl;
+
+	return 0;
+}
