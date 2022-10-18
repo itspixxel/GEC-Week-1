c1f11997cfada270af9c9f52ae733b9ad2172754
itspixxel <s029518m@student.staffs.ac.uk>
2022-10-18 20:03:42 +0100

    Added Prog13_Discount

---
+++ Prog13_Discount/Prog13_Discount.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char userStatus;
+	int userChoice;
+
+	cout << "Please tell me your status with 's' for Student, 't' for Teacher, or 'o' for Other" << endl;
+	cin >> userStatus;
+
+	cout << "Which game would you like?" << endl;
+	cin >> userChoice;
+
+	if (userStatus == 's' || userStatus == 't')
+	{
+		if (userChoice == 1)
+		{
+			cout << "You are eligible for a 20% discount!" << endl;
+		}
+		else if (userChoice == 2)
+		{
+			cout << "You are eligible for a 10% discount!" << endl;
+		}
+	}
+	else 
+	{
+		cout << "You are not entitled to a discount." << endl;
+	}
+}
