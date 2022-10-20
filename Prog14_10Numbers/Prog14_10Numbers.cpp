67cd3417a681d8ef1b3b9d2e0b8c49eb55b0b071
itspixxel <s029518m@student.staffs.ac.uk>
2022-10-20 15:47:41 +0100

    Added Prog14_10Numbers

---
+++ Prog14_10Numbers/Prog14_10Numbers.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int inputNum = 0;
+	int total = 0;
+	float mean = 0.0f;
+	int numCount = 2;
+
+	cout << "Enter a number" << endl;
+	cin >> inputNum;
+	total += inputNum;
+
+	cout << endl;
+
+	cout << "Enter a number" << endl;
+	cin >> inputNum;
+	total += inputNum;
+
+	mean = (float)total / numCount;
+
+	cout << "Sum: " << total << endl;
+	cout << "Mean: " << mean << endl;
+
+	do
+	{
+		cout << endl;
+
+		cout << "Enter a number" << endl;
+		cin >> inputNum;
+
+		if (inputNum != 0)
+		{
+			numCount++;
+			total += inputNum;
+			mean = (float)total / numCount;
+
+			cout << endl;
+
+			cout << "Sum: " << total << endl;
+			cout << "Mean: " << mean << endl;
+		}
+	} while (inputNum != 0);
+	cout << "..exit" << endl;
+}
