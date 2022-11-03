681379e5e47dbe3c06e285d9868b205ffb355415
itspixxel <s029518m@student.staffs.ac.uk>
2022-11-03 16:19:23 +0000

    Added Prog19_oddsAndEvens

---
+++ Prog19_oddsAndEvens/Prog19_oddsAndEvens.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+void outputResults(int numOfOdd, int oddTotal, int numOfEven, int evenTotal);
+bool isOddorEven(int num);
+
+int main()
+{
+	int number;
+	int oddsTotal = 0;
+	int evensTotal = 0;
+	int evens = 0;
+	int odds = 0;
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Please enter value " << i+1 << endl;
+		cin >> number;
+		if (isOddorEven(number))
+		{
+			evensTotal += number;
+			evens++;
+			cout << "This value is even" << endl;
+		}
+		else
+		{
+			oddsTotal += number;
+			odds++;
+			cout << "This value is odd" << endl;
+		}
+	}
+	outputResults(odds, oddsTotal, evens, evensTotal);
+}
+
+bool isOddorEven(int num)
+{
+	if (num % 2 == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void outputResults(int numOfOdd, int oddTotal, int numOfEven, int evenTotal)
+{
+	cout << "You entered " << numOfOdd << " odd values.\n Adding up to a total of " << oddTotal << endl;
+	cout << "You entered " << numOfEven << " even values.\n Adding up to a total of " << evenTotal << endl;
+}
