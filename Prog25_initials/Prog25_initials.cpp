871fb89f93f73ef82a362c76c360301470ffb93d
itspixxel <s029518m@student.staffs.ac.uk>
2022-11-10 16:56:27 +0000

    Added Prog25_initials

---
+++ Prog25_initials/Prog25_initials.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char firstMidSurArr[256];
+	cin.get(firstMidSurArr, 255);
+
+	for (int i = 0; i < size(firstMidSurArr); i++)
+	{
+		if (firstMidSurArr[i] == ' ')
+		{
+			cout << "Found space" << endl;
+		}
+	}
+}
