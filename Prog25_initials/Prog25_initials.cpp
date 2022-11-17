56baabb3bb6fcca65d7a5d142021ff95a6db3ef9
itspixxel <s029518m@student.staffs.ac.uk>
2022-11-17 16:07:41 +0000

    Update Prog25_initials.cpp

--- Prog25_initials/Prog25_initials.cpp
+++ Prog25_initials/Prog25_initials.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main()
 {
-	char firstMidSurArr[256];
+	char firstMidSurArr[256] = "";
+	cout << "Please enter your first name inital followed by your middle name initial followed by your full last name." << endl;
 	cin.get(firstMidSurArr, 255);
 
 	for (int i = 0; i < size(firstMidSurArr); i++)
 	{
+		cout << firstMidSurArr[i];
 		if (firstMidSurArr[i] == ' ')
 		{
-
+			cout << endl;
 		}
 	}
 }
