8d7f6547b853efece0ce5c42608fa39e4bb29f0c
itspixxel <s029518m@student.staffs.ac.uk>
2022-12-01 15:11:08 +0000

    mushy keys

---
+++ Prog36_theChangingPointer/Prog36_theChangingPointer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void inputDetails(int* n1, int* n2);
+void outputDetails(int& integer1, int& integer2, int* pointer);
+
+int main()
+{
+	int num1;
+	int num2;
+
+
+}
